5a92e3b8a85181abe6766c9571996f2e33b3af84
mr Ramy <mr.rami.bendhia@gmail.com>
2024-04-14 20:26:05 +0100

    new script on the network to fix the mirroring

--- Assets/New Version/MyMirrorNetworkScript.cs
+++ Assets/New Version/MyMirrorNetworkScript.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 using Mirror;
 public class MyMirrorNetworkScript : NetworkBehaviour 
 {
-    //public let[] listToDisable = new var[0];
     // Start is called before the first frame update
     public GameObject parent;
+    public Component[] listToDisable;
     void Start()
     {
         
@@ -18,12 +18,21 @@ public class MyMirrorNetworkScript : NetworkBehaviour
         if (!isLocalPlayer)
         {
 
-            //foreach (GameObject item in listToDisable)
-            //{
-            //    item.SetActive(false);
-            //}
+            foreach (Component component in listToDisable)
+            {
+                DisableAllScripts(component.gameObject);
+            }
             parent.SetActive(false);
             return;
+        }
+    }
+
+    void DisableAllScripts(GameObject obj)
+    {
+        MonoBehaviour[] scripts = obj.GetComponents<MonoBehaviour>();
+        foreach (MonoBehaviour script in scripts)
+        {
+            script.enabled = false;
         }
     }
 }
